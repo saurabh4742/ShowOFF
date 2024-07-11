cc3b1438fe06bf7b9829637d182e71d12677484a
Saurabh Anand <128067046+saurabh4742@users.noreply.github.com>
2024-07-11 20:54:19 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,10 +46,12 @@ export default function RootLayout({
           <SocketProvider>
           <Toaster/>
           <NewResponsiveNav/>
+<div className="min-h-[80vh]">
           {children}
+</div>
           </SocketProvider>
           <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-muted-foreground">&copy; 2024 Jobfinder. All rights reserved.</p>
+        <p className="text-xs text-muted-foreground">&copy; 2024 ShowOff by Saurabh Anand. All rights reserved.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link href="#" className="text-xs hover:underline underline-offset-4 text-muted-foreground" prefetch={false}>
             Terms of Service
